83d2390f9ecdbafdec08a5b02e95bab0af8401d6
Steve Lazaridis <steve@penguincoders.com>
2015-11-16 23:21:59 -0500

    added expressjs, added canvasjs barchart to display changes in temp and humidity

--- app.js
+++ app.js
@@ -1,51 +1,59 @@
+var express = require('express');
 var SerialPort = require("serialport").SerialPort;
 var serial = new SerialPort("/dev/ttyUSB0", { baudrate: 115200 });
-
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
 var fs = require('fs');
+var app = express();
 
 serial.open(function(error) {
     if (error) {
         console.log('failed to open: ' + error);
     } else {
         console.log('Opened serial port.');
-        var begin = false;
-        var end = false;
-        var data_str = "";
+        var json_begin = false;
+        var json_end = false;
+        var json_data_str = "";
 
         serial.on('data', function(data) {
             var str = data.toString();
             for (i=0; i<str.length; i++) {
                 var c = str[i];
-                data_str += c;
-                if (c == "{") { begin = true; }
-                if (c == "}") { end = true; }
+                json_data_str += c;
+                if (c == "{") { json_begin = true; }
+                if (c == "}") { json_end = true; }
 
-                if (begin == true && end == true) {
-                    io.emit('sensor_data', data_str);
-                    console.log("Got: " + data_str);
-                    data_str = "";
-                    begin = false;
-                    end = false;
+                /* Do we have a full json object from the serial port? */
+                if (json_begin == true && json_end == true) {
+                    var d = new Date();
+                    console.log(d.getTime() + " - " + json_data_str);
+                    io.emit('sensor_data', json_data_str);
+                    json_data_str = "";
+                    json_begin = false;
+                    json_end = false;
                 }
             }
         });
     }
 });
 
-app.listen(8080);
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+  //res.send('Hello World!');
+  //fs.readFile(__dirname + '/index.html',
+  fs.readFile(__dirname + '/index_new.html',
   function (err, data) {
     if (err) {
       res.writeHead(500);
-      return res.end('Error loading index.html');
+      return res.end('Error loading index');
     }
 
     res.writeHead(200);
     res.end(data);
   });
-}
+});
 
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Listening at http://%s:%s', host, port);
+});
+var io = require('socket.io')(server);
